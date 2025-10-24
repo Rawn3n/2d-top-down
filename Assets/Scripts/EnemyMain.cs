43d9c5d26afcfec0f2262ae89df25fccbd84a08a
Rawn3n <129275096+Rawn3n@users.noreply.github.com>
2025-10-24 12:13:19 +0200

    ss

---
+++ Assets/Scripts/EnemyMain.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public abstract class EnemyMain : MonoBehaviour
+{
+    public int damage = 10;
+    public int health = 100;
+    public float speed = 5.0f;
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
